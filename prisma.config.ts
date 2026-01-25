e96c74f7d6b3a2ce5e9cc0495a9f31f97d14ba02
DavidQRy <davidgui393@gmail.com>
2026-01-24 23:29:25 -0500

    fix: solution error seeds

--- prisma.config.ts
+++ prisma.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
-      seed: './prisma/seed.ts',
+      seed: 'tsx prisma/seed.ts',
   },
   datasource: {
     url: process.env["DATABASE_URL"],
